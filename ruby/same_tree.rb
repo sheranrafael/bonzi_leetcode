22033841d212da96c15708dcc680b3c37f782546
Rafael Sheran <154270131+sheranrafael@users.noreply.github.com>
2025-10-19 16:09:22 -0300

    leetcode-100

---
+++ ruby/same_tree.rb
@@ -0,0 +1,8 @@
+# LeetCode #100: Same Tree (Ruby)
+def is_same_tree(p, q)
+
+    return true if p.nil? && q.nil?
+    return false if p.nil? || q.nil?
+    return false if p.val != q.val
+    is_same_tree(p.left, q.left) && is_same_tree(p.right, q.right)
+end
